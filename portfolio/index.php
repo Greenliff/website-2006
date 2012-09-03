157a6d664448c70934358baaf347b28793bcd4f7
Juraj Skripsky <jskripsky@greenliff.com>
2012-09-03 17:44:50 +0200

    Clean up.

--- portfolio/index.php
+++ portfolio/index.php
@@ -5,28 +5,16 @@ $datei = $array[2];
 $array = explode('.', $datei);
 $datei = $array[0];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
     <title>Greenliff: Service Portfolio</title>
-		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-		<meta name="description" content="Our software- testing services are measure-tailored. Our offering includes testing services customizable to your needs. The services range from handling a single test project to full responsibility of your testing activities on an ongoing basis." />
-		<meta name="robots" content="index,follow" />
+    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
+    <meta name="description" content="Our software- testing services are measure-tailored. Our offering includes testing services customizable to your needs. The services range from handling a single test project to full responsibility of your testing activities on an ongoing basis." />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/portfolio.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <!--[if IE 5]>
-    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 6]>
-    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_once("../includes/google-analytics.html"); ?>
   </head>
