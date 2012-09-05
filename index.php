78d1bb3209157ac068175178e00769c5393ac6bb
Juraj Skripsky <js@hotfeet.ch>
2012-09-05 12:58:56 +0200

    Radical clean up.

--- index.php
+++ index.php
@@ -4,53 +4,64 @@ $datei = "index";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>Software testing: quality control, consultancy, engineering, automation</title>
-    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-    <meta name="description" content="We are an independent engineering and service company focused on software testing and quality control. We offer test consultancy, engineering and automation." />
-    <meta name="language" content="en" />
-    <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_once("includes/google-analytics.html"); ?>
-  </head>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+		<title>Software testing: quality control, consultancy, engineering, automation</title>
+		<meta name="description" content="We are an independent engineering and service company focused on software testing and quality control. We offer test consultancy, engineering and automation." />
+		<meta name="language" content="en" />
+		<link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
+		<link rel="shortcut icon" href="/favicon.ico" />
+		<?php include_once("includes/google-analytics.html"); ?>
+	</head>
 
-  <body>
-    <div id="abstand"></div>
-    <div id="container">
+	<body>
+		<div id="abstand"></div>
+		<div id="container">
 <?php include_once("includes/printhead.php"); ?>
-      <div id="left">
-      <?php include_once("includes/navigation.php"); ?>
-      </div>
+			<div id="left">
+			<?php include_once("includes/navigation.php"); ?>
+			</div>
 
-      <div id="middle">&nbsp;</div>
+			<div id="middle">&nbsp;</div>
 
-      <div id="content" class="home">
-        <div class="startspalte">
-        <h1>Software Testing is our core competence</h1>
-        <br />
-          <p>Greenliff is an independent engineering and service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical, and the financial sectors, we specialize in test consultancy, test automation, and the development of test solutions covering the entire software life cycle from specification to maintenance.</p>
+			<div id="content" class="home">
+				<div class="startspalte">
+				<h1>Software Testing is our core competence</h1>
+				<br />
+					<p>
+						Greenliff is an independent engineering and service company focused on software testing and quality control.
+						Serving customers in the telecom, the industrial, the medical, and the financial sectors, we specialize in 
+						test consultancy, test automation, and the development of test solutions covering the entire software life
+						cycle from specification to maintenance.
+					</p>
 
-          <p>Software testing is a <i>technical audit discipline</i> to assure function and quality. It is in no way an intuitive, but a highly specialized and extremely demanding discipline of IT. Although the importance of software testing is widely accepted, the required skills and effort is often underestimated. Not so at Greenliff: our consultants bring the needed expertise in both quality standards and software engineering best practices.</p>
+					<p>
+						Software testing is a <i>technical audit discipline</i> to assure function and quality. It is in no way an 
+						intuitive, but a highly specialized and extremely demanding discipline of IT. Although the importance of 
+						software testing is widely accepted, the required skills and effort is often underestimated.
+						Not so at Greenliff: our consultants bring the needed expertise in both quality standards and software
+						engineering best practices.
+					</p>
 
-          <p>As an independent and neutral 3rd party, we are ready to manage, design, implement, and execute all your tests.</p>
-        </div>
+					<p>
+						As an independent and neutral 3rd party, we are ready to manage, design, implement, and execute all your tests.
+					</p>
+				</div>
 
-        <div class="news">
-          <h2>News</h2>
-          <div class="bildlinks" style="padding:5px;">
-            <a href="/company/jobs.php"><img src="images/greenliff_hiring.gif" alt="road sign: men at work" width="167" height="53" /></a>
-          </div>
-          <p style="clear:left"><strong>Greenliff is hiring</strong><br />
-          We are currently seeking a Software Engineer C# (m/w) at our Zurich Technopark office. Apply now.<br />
-          For more information click <a href="docs/20120713_GE_SoftwareEngineer.pdf">here</a>.</p>
-        </div>
+				<div class="news">
+					<h2>News</h2>
+					<div class="bildlinks" style="padding:5px;">
+						<a href="/company/jobs.php"><img src="images/greenliff_hiring.gif" alt="road sign: men at work" width="167" height="53" /></a>
+					</div>
+					<p style="clear:left"><strong>Greenliff is hiring</strong><br />
+					We are currently seeking a Software Engineer C# (m/w) at our Zurich Technopark office. Apply now.<br />
+					For more information click <a href="docs/20120713_GE_SoftwareEngineer.pdf">here</a>.</p>
+				</div>
 
-      </div>
+			</div>
 <?php include_once("includes/footer.html"); ?>
-    </div>
-  </body>
+		</div>
+	</body>
 </html>
 
 
