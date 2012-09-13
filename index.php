a14b5bf1702f6d06c024674a6c192da7149726cd
Juraj Skripsky <js@hotfeet.ch>
2012-09-13 23:42:06 +0200

    Remove <br />, remove some commas.

--- index.php
+++ index.php
@@ -26,12 +26,11 @@ $datei = "index";
 
 			<div id="content" class="home">
 				<div class="startspalte">
-				<h1>Software Testing is our core competence</h1>
-				<br />
+					<h1>Software Testing is our core competence</h1>
 					<p>
 						Greenliff is an independent engineering and service company focused on software testing and quality control.
-						Serving customers in the telecom, the industrial, the medical, and the financial sectors, we specialize in 
-						test consultancy, test automation, and the development of test solutions covering the entire software life
+						Serving customers in the telecom, the industrial, the medical and the financial sectors, we specialize in 
+						test consultancy, test automation and the development of test solutions covering the entire software life
 						cycle from specification to maintenance.
 					</p>
 
@@ -39,12 +38,11 @@ $datei = "index";
 						Software testing is a <i>technical audit discipline</i> to assure function and quality. It is in no way an 
 						intuitive, but a highly specialized and extremely demanding discipline of IT. Although the importance of 
 						software testing is widely accepted, the required skills and effort is often underestimated.
-						Not so at Greenliff: our consultants bring the needed expertise in both quality standards and software
-						engineering best practices.
+						Our consultants bring the needed expertise in both quality standards and software engineering best practices.
 					</p>
 
 					<p>
-						As an independent and neutral 3rd party, we are ready to manage, design, implement, and execute all your tests.
+						As an independent and neutral 3rd party, we are ready to manage, design, implement and execute all your tests.
 					</p>
 				</div>
 
