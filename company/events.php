74f219c7b59b76417b03c357e844b1ddea38ab7b
Juraj Skripsky <jskripsky@greenliff.com>
2012-09-13 11:06:58 +0200

    Clean more page titles.

--- company/events.php
+++ company/events.php
@@ -9,9 +9,9 @@ $datei = $array[0];
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>Greenliff: Events</title>
+    <title>Upcoming Events</title>
     <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-    <meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+    <meta name="description" content="" />
     <meta name="language" content="en" />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
--- company/request.php
+++ company/request.php
@@ -122,22 +122,12 @@ request you as soon as possible.";
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>    
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>Greenliff: Online request</title> 
-		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+    <title>Request more information - company brochure, mailing list</title> 
+    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
+    <meta name="description" content="" />
     <meta name="language" content="en" />
-		<meta name="robots" content="index,follow" />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <!--[if IE 5]>
-    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]--> 
-    <!--[if IE 6]>
-    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]--> 
-    <!--[if IE 7]>
-    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_once("../includes/google-analytics.html"); ?>
   </head>
