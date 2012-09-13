70e11aaad10be82436d255002c4b74d27a83b339
Juraj Skripsky <jskripsky@greenliff.com>
2012-09-13 11:13:13 +0200

    Another round of clean ups.

--- courses/index.php
+++ courses/index.php
@@ -5,29 +5,17 @@ $datei = $array[2];
 $array = explode('.', $datei);
 $datei = $array[0];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>    
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>Greenliff: Certified Tester</title> 
-		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+    <title>Certified Tester</title> 
+    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
+    <meta name="description" content="" />
     <meta name="language" content="en" />
-		<meta name="robots" content="index,follow" />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/courses.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <!--[if IE 5]>
-    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]--> 
-    <!--[if IE 6]>
-    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]--> 
-    <!--[if IE 7]>
-    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_once("../includes/google-analytics.html"); ?>
   </head>
