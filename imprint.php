eb79966515fe36a1b3a52376cdd0f66173f8b31d
Juraj Skripsky <js@hotfeet.ch>
2012-09-15 14:55:24 +0200

    Refactor index and imprint.

--- imprint.php
+++ imprint.php
@@ -1,65 +1,42 @@
-<?php
-$pfad = "";
-$datei = "impressum";
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>    
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-    <title>Greenliff AG in Technopark Zurich, Switzerland</title>
-    <!--<meta name="description" content="" />-->
-    <link rel="stylesheet" type="text/css" href="styles/screen.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_once("includes/google-analytics.html"); ?>
-  </head>
-  
-  <body>
-    <div id="abstand"></div>
-    <div id="container">
-<?php include_once("includes/printhead.php"); ?>
-      <div id="left">
-<?php include_once("includes/navigation.php"); ?>
-      </div>
-      
-      <div id="middle">&nbsp;</div>
-      
-      <div id="content" class="home">
-        <div class="impressumspalte">
-          <h1>Imprint</h1>
-          <p>Greenliff AG<br />
-          Technoparkstrasse 1<br />
-          8005 Zürich<br />
-          Switzerland</p>
-          
-          <p><span class="anschrift">Phone</span>+41 43 20 40 800<br />
-          <span class="anschrift">Fax</span>+41 43 20 40 808</p>
-          
-          <p><span class="anschrift">Email</span><a href="mailto:&#105;&#110;&#102;&#111;&#64;&#103;&#114;&#101;&#101;&#110;&#108;&#105;&#102;&#102;&#46;&#99;&#111;&#109;">&#105;&#110;&#102;&#111;&#64;&#103;&#114;&#101;&#101;&#110;&#108;&#105;&#102;&#102;&#46;&#99;&#111;&#109;</a></p>
-          <p><span class="anschrift">Firmennummer</span> CH-020.3.027.535-6<br />
-          <span class="anschrift">&nbsp;</span>Handelsregister Zürich<br />
-          <span class="anschrift">Mwst-Nr.</span>588 442</p>
-          
-          <p><span class="anschrift" style="height:66px;">Validation</span>
-            <a href="http://validator.w3.org/check?uri=referer">
-              <img src="/images/valid-xhtml10.png" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
-            </a><br />
-            <a href="http://jigsaw.w3.org/css-validator/">
-              <img style="width:88px;height:31px" src="/images/vcss.png" alt="Valid CSS!" />
-           </a>
-          </p>
-        </div>
-        
-        <div class="spalte2">
-          <h2 class="abstandOben">Web Design</h2>
-          <p><a href="http://www.x-crossover.de/" target="_blank">crossover</a></p>
-          
-          <h2>Implementation</h2>
-          <p><a href="http://www.netweaver.ch/" target="_blank">Burri Webdesign</a></p>
-        </div>
-      </div>
-<?php include_once("includes/footer.html"); ?>
-    </div>
-  </body>
-</html>
+<?
+	$title = "Greenliff AG in Technopark Zurich, Switzerland";
+	$desc = "";
 
+	include_once("includes/header.inc.php");
+?>
+<div id="content" class="home">
+  <div class="impressumspalte">
+    <h1>Imprint</h1>
+    <p>Greenliff AG<br />
+    Technoparkstrasse 1<br />
+    8005 Zürich<br />
+    Switzerland</p>
+    
+    <p><span class="anschrift">Phone</span>+41 43 20 40 800<br />
+    <span class="anschrift">Fax</span>+41 43 20 40 808</p>
+    
+    <p><span class="anschrift">Email</span><a href="mailto:&#105;&#110;&#102;&#111;&#64;&#103;&#114;&#101;&#101;&#110;&#108;&#105;&#102;&#102;&#46;&#99;&#111;&#109;">&#105;&#110;&#102;&#111;&#64;&#103;&#114;&#101;&#101;&#110;&#108;&#105;&#102;&#102;&#46;&#99;&#111;&#109;</a></p>
+    <p><span class="anschrift">Firmennummer</span> CH-020.3.027.535-6<br />
+    <span class="anschrift">&nbsp;</span>Handelsregister Zürich<br />
+    <span class="anschrift">Mwst-Nr.</span>588 442</p>
+    
+    <p><span class="anschrift" style="height:66px;">Validation</span>
+      <a href="http://validator.w3.org/check?uri=referer">
+        <img src="/images/valid-xhtml10.png" alt="Valid XHTML 1.0 Transitional" />
+      </a><br />
+      <a href="http://jigsaw.w3.org/css-validator/">
+        <img src="/images/vcss.png" alt="Valid CSS" />
+     </a>
+    </p>
+  </div>
+  
+  <div class="spalte2">
+    <h2 class="abstandOben">Web Design</h2>
+    <p><a href="http://www.x-crossover.de/" target="_blank">crossover</a></p>
+    
+    <h2>Implementation</h2>
+    <p><a href="http://www.netweaver.ch/" target="_blank">Burri Webdesign</a></p>
+  </div>
+</div>
+
+<? include_once("includes/footer.inc.php"); ?>
