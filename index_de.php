95e491ee53115f0bd17bd838a3904e23a1d849ca
Juraj Skripsky <js@hotfeet.ch>
2012-09-15 14:56:33 +0200

    Add german index.

---
+++ index_de.php
@@ -0,0 +1,42 @@
+<?
+	$title = "Software testing: Qualitätssicherung, Beratung, Entwicklung, Automation";
+	$desc = "Wir sind ein We are an independent engineering and service company focused on software testing and quality control. We offer test consultancy, engineering and automation.";
+
+	include_once("includes/header.inc.php");
+?>
+<div id="content" class="home">
+	<div class="startspalte">
+		<h1>Software Testing ist unsere Kernkompetenz</h1>
+		<p>
+			Greenliff ist ein unabhängiges Ingenieur- und Service-Unternehmen, das sich Software Testing und Qualitätssicherung konzentriert.
+			Für Kunden in der Telekommunikation, im Industrie-, Medizinal- und Finanzsektor, spezialisieren wir uns im Test Beratung, Testautomatisierung 
+			und der Entwicklung von Testlösungen für den gesamten Software-Lebenszyklus von der Spezifikation bis zur Wartung.
+		</p>
+
+		<p>
+			Software-Test ist ein technisches Audit Disziplin Funktion und Qualität zu gewährleisten.
+			Es ist in keiner Weise eine intuitive, sondern eine hoch spezialisierte und sehr anspruchsvolle Disziplin der IT.
+			Obwohl die Bedeutung von Software-Test allgemein akzeptiert wird, werden die erforderlichen Fähigkeiten und 
+			Anstrengungen oft unterschätzt. Unsere Berater bringen die notwendige Expertise in beiden Qualitätsstandards 
+			und Software-Engineering Best Practices.
+		</p>
+
+		<p>
+			Als unabhängige und neutrale dritte Partei, sind wir bereit, zu verwalten, zu entwerfen, zu implementieren und ausführen alle Tests.		
+		</p>
+	</div>
+
+	<div class="news">
+		<h2>News</h2>
+		<div class="bildlinks" style="padding:5px;">
+			<a href="/company/jobs.php"><img src="images/greenliff_hiring.gif" alt="We're hiring" /></a>
+		</div>
+		<p style="clear:left"><strong>Wir suchen</strong><br />
+			Wir suchen einen <a href="docs/20120713_GE_SoftwareEngineer.pdf">Software Ingenieur C# (m/w)</a> in unserer Filiale im Technopark Zürich. Bewerbenen Sie sich jetzt.
+		</p>
+	</div>
+</div>
+
+<? include_once("includes/footer.inc.php"); ?>
+
+
