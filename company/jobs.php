31ea8135d3c932990f2bc202587b167bfb6716b7
Juraj Skripsky <js@hotfeet.ch>
2012-09-15 15:03:37 +0200

    Refactor jobs.php.

--- company/jobs.php
+++ company/jobs.php
@@ -1,35 +1,9 @@
-<?php
-$array = explode ('/', $_SERVER['PHP_SELF']);
-$pfad = $array[1];
-$datei = $array[2];
-$array = explode('.', $datei);
-$datei = $array[0];
+<?
+	$title = "Software Testing, Test Consultancy, Automation, Performance Testing, Zurich";
+	$desc = "We are a fast growing company that manages challenging software testing projects. We are constantly looking for highly qualified individuals.";
 
-require_once("../includes/funktionen.inc.php");
+	include_once("../includes/header.inc.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-    <title>Software Testing, Test Consultancy, Automation, Performance Testing, Zurich</title>
-    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-    <meta name="description" content="We are a fast growing company that manages challenging software testing projects. We are constantly looking for highly qualified individuals." />
-    <meta name="language" content="en" />
-    <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
-    <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_once("../includes/google-analytics.html"); ?>
-  </head>
-
-  <body>
-    <div id="abstand"></div>
-    <div id="container">
-<?php include_once("../includes/printhead.php"); ?>
-      <div id="left">
-      <?php include_once("../includes/navigation.php"); ?>
-      </div>
-      <div id="middle">
-      </div>
       <div id="content" class="home">
         <div class="spalte1">
           <h1>Jobs</h1>
@@ -44,16 +18,11 @@ require_once("../includes/funktionen.inc.php");
           <h1>We are hiring</h1>
 
           <p>We are currently looking for motivated individuals to fill the following openings:</p>
-          <!--<p>Currently no open positions.</p>-->
           <h2>Zurich</h2>
           <ul>
             <li><a href="../docs/20110503_EN_ManualTester_TestAnalyst.pdf" target="_blank">Manual Tester / Test Analyst</a></li>
             <li><a href="../docs/20120713_GE_SoftwareEngineer.pdf" target="_blank">Software Engineer C# (m/w)</a></li>
           </ul>
-          <!--<h2>Bern/Lausanne</h2>
-          <ul>
-            <li><a href="../docs/20090407_Test_Engineer_Bern.pdf" target="_blank">Test Engineer</a></li>
-          </ul>-->
          </div>
 
          <div class="news">
@@ -63,7 +32,5 @@ require_once("../includes/funktionen.inc.php");
           <p style="padding:3px 5px;">Please send your curriculum vitae preferably by email to <a href="<?php echo htmlencode("mailto:jobs@greenliff.com"); ?>"><?php echo htmlencode("jobs@greenliff.com"); ?></a></p>
          </div>
       </div>
-<?php include_once("../includes/footer.html"); ?>
-    </div>
-  </body>
-</html>
+
+<? include_once("../includes/footer.inc.php"); ?>
