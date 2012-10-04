6f7d3324847fd4bc96ccf071b77270044c70839a
Juraj Skripsky <jskripsky@greenliff.com>
2012-10-04 12:07:38 +0200

    Add new files added outside of git.

---
+++ company/121120_sign-in.php
@@ -0,0 +1,251 @@
+<?php
+$array = explode ('/', $_SERVER['PHP_SELF']);
+$pfad = $array[1];
+$datei = $array[2];
+$array = explode('.', $datei);
+$datei = $array[0];
+require_once("../includes/funktionen.inc.php");
+
+// Variablen initialisieren
+$empfaenger = 'events@greenliff.com';
+//$empfaenger = 'uburri@netweaver.ch';
+$success = false;
+$fehler = false;
+$readonly = '';
+$title = '';
+$firstname = '';
+$lastname  = '';
+$company = '';
+$department = '';
+$phone = '';
+$fax = '';
+$e_mail = '';
+$street = '';
+$zip = '';
+$city = '';
+$comment = '';
+
+
+if(isset($_REQUEST))
+{
+  $missbrauch = formularmissbrauch($_REQUEST);
+}
+// Courses
+if(isset($_POST['send']))
+{
+  // Formularmissbraeuche abfangen
+  $_POST = array_map('_filterData',$_POST);
+
+  if(isset($_POST['title']))
+  {
+    $title = $_POST['title'];
+  }
+  $firstname = $_POST['firstname'];
+  $lastname  = $_POST['lastname'];
+  $company = $_POST['company'];
+  $department = $_POST['department'];
+  $phone = $_POST['phone'];
+  $fax = $_POST['fax'];
+  $e_mail = $_POST['e_mail'];
+  $street = $_POST['street'];
+  $zip = $_POST['zip'];
+  $city = $_POST['city'];
+  $comment = $_POST['comment'];
+
+  // fehlende Daten
+  if($firstname == '' || $lastname == '' || $company == '' || $phone == '' || $e_mail == '' || $street == '' || $zip == '' || $city == '')
+  {
+    $meldung = "Bitte füllen Sie alle benötigten Daten aus.";
+    $fehler = true;
+  }
+  // E-Mail ist formal nicht korrekt
+  else if(!teste_email($e_mail))
+  {
+    $meldung = "Ungültige E-Mail-Adresse.";
+    $fehler = true;
+  }
+  else
+  {
+    $extra = "MIME-Version: 1.0\nContent-Type: text/plain; charset=iso-8859-1\nContent-Transfer-Encoding: 8-bit\n";
+    $betreff = "Anmeldung 20. November 2012: Agile Software-Projekte";
+    $text = "\n\n";
+    $text .= 'Anmeldung für den Anlass "Brauchen Agile Software-Projekte eine Prozessverbesserung?" vom 20. November 2012' . ":\n\n";
+    $text .= "Anmeldedatum...: " . date("d.m.Y") . "\n";
+    $text .= "Anrede.........: " . $title . "\n";
+    $text .= "Name...........: " . $firstname . " " . $lastname . "\n";
+    if($company != "")
+    {
+    $text .= "Firma..........: " . $company . "\n";
+    }
+    if($department != "")
+    {
+      $text .= "Abteilung......: " . $department . "\n";
+    }
+    $text .= "Strasse........: " . $street . "\n";
+    $text .= "PLZ/Ort........: " . $zip . " " . $city . "\n";
+    $text .= "E-Mail-Addresse: " . $e_mail . "\n";
+    $text .= "Telefon........: " . $phone;
+    if($fax != "")
+    {
+      $text .= "\nFax............: " . $fax;
+    }
+    if($comment != "")
+    {
+      $text .= "\n\nMitteilung: \n" . $comment;
+    }
+    // print_r($text);
+  	// E-Mail-Versand
+    if(!$missbrauch)
+    {
+      /* Baut Header der Mail zusammen */
+      $headers = 'From:' . $empfaenger . "\n";
+      $headers .= 'X-Mailer: PHP/' . phpversion() . "\n";
+      $headers .= 'X-Sender-IP: ' . $REMOTE_ADDR . "\n";
+      $headers .= "Content-type: text/plain; charset=iso-8859-1\n";
+      $headers .= "Content-Transfer-Encoding: 8-bit\n";
+      $headers .= 'Cc: ' . $empfaenger . "\n";
+//echo nl2br($text);
+    	if(@mail($e_mail,$betreff,$text,$headers))
+      {
+        $meldung = "<p>Vielen Dank für Ihre Anmeldung.</p>\n<p>Sie erhalten eine Kopie Ihrer Anmeldung per Mail.</p>";
+        $success = true;
+        $readonly = ' readonly="readonly"';
+      }
+      else {
+        $meldung = 'Ihre Anmeldung konnte nicht abgeschickt werden. Bitte <a href="mailto:info@greenliff.com">informieren Sie uns</a> über das Problem, so dass wir es beheben und Sie für den Anlass registrieren können.';
+        $fehler = true;
+      }
+    }
+
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <title>Greenliff: Brauchen Agile Software-Projekte eine Prozessverbesserung? - Anmeldung</title>
+		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
+		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+    <meta name="language" content="en" />
+		<meta name="robots" content="index,follow" />
+    <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
+    <!--[if IE 5]>
+    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <!--[if IE 6]>
+    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <!--[if IE 7]>
+    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <link rel="shortcut icon" href="/favicon.ico" />
+  </head>
+
+  <body>
+    <div id="abstand"></div>
+    <div id="container">
+<?php include_once("../includes/printhead.php"); ?>
+      <div id="left">
+      <?php include_once("../includes/navigation.php"); ?>
+      </div>
+      <div id="middle">
+      </div>
+      <div id="scrollcontent" class="events">
+        <div class="startspalte">
+          <h1>Anmeldung für die Veranstaltung</h1>
+          <h2>&laquo;Brauchen Agile Software-Projekte eine Prozessverbesserung?&raquo;</h2>
+
+          <a name="Address"></a>
+          <form method="post" action="<?php echo $datei; ?>.php" style="margin-top: 30px;">
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $title == "") echo " marker"; ?>"><!-- Title --></span><input type="radio" name="title" value="Herr" <?php if($title == "Herr") { echo ' checked="checked"'; } ?> /><span class="<?php if($fehler && $title == "") echo " marker"; ?>">Herr</span> <input type="radio" name="title" value="Frau" <?php if($title == "Frau") { echo ' checked="checked"'; } ?> style="margin-left:20px;" /><span class="<?php if($fehler && $title == "") echo " marker"; ?>">Frau</span>
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $firstname == "") echo " marker"; ?>">Name</span><input type="text" name="firstname"<?php echo $readonly; ?> class="formfeld" value="<?php echo $firstname; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $lastname == "") echo " marker"; ?>">Vorname</span><input type="text" name="lastname"<?php echo $readonly; ?> class="formfeld" value="<?php echo $lastname; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $company == "") echo " marker"; ?>">Firma</span><input type="text" name="company"<?php echo $readonly; ?> class="formfeld" value="<?php echo $company; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel">Abteilung/Funktion</span><input type="text" name="department"<?php echo $readonly; ?> class="formfeld" value="<?php echo $department; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $phone == "") echo " marker"; ?>">Telefon</span><input type="text" name="phone"<?php echo $readonly; ?> class="formfeld" value="<?php echo $phone; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel">Fax</span><input type="text" name="fax"<?php echo $readonly; ?> class="formfeld" value="<?php echo $fax; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $e_mail == "") echo " marker"; ?>">E-Mail</span><input type="text" name="e_mail"<?php echo $readonly; ?> class="formfeld" value="<?php echo $e_mail; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $street == "") echo " marker"; ?>">Strasse</span><input type="text" name="street"<?php echo $readonly; ?> class="formfeld" value="<?php echo $street; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $zip == "") echo " marker"; ?>">PLZ</span><input type="text" name="zip"<?php echo $readonly; ?> class="formfeldkurz" value="<?php echo $zip; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $city == "") echo " marker"; ?>">Ort</span><input type="text" name="city"<?php echo $readonly; ?> class="formfeld" value="<?php echo $city; ?>" />
+            </div>
+            <div class="div1" style="margin-top: 30px;">
+              <span class="formlabel">Bemerkung</span><textarea name="comment"<?php echo $readonly; ?> class="textarea" style="height:60px;"><?php echo $comment; ?></textarea>
+            </div>
+<?php
+if(!$success)
+{
+?>
+            <div class="div2">
+            <input type="submit" name="send" value="send" class="submit" />
+            </div>
+           </form>
+<?php
+}
+?>
+        </div>
+<?php
+if(!isset($_POST["send"]))
+{
+?>
+        <div class="news">
+          <h3>20. November 2012</h3>
+          <p>Technopark Zürich, Technoparkstr. 1, 8005 Zürich</p>
+          <p>14:00 - 17:00 Uhr mit anschliessendem Apéro</p>
+          <p>Referenten: Dr. Ernest Wallmüller, Carl Worms, Martin Voss</p>
+          <p>Die Teilnahme ist kostenlos. Eine Buchungsbestätigung erfolgt in der Reihenfolge der Anmeldung. </p>
+          <p>Bitte berücksichtigen Sie, dass die Anzahl der Plätze limitiert ist.</p>
+          <p><a href="../docs/Agile_Software_Qualitaetsverbesserung_14092012.pdf" target="_blank">Veranstaltungsflyer</a> (pdf)</p>
+
+        </div>
+<?php
+}
+else if($fehler)
+{
+?>
+        <div class="news">
+          <h3>Registration</h3>
+          <p><?php echo $meldung; ?></p>
+        </div>
+<?php
+}
+else if($success)
+{
+?>
+        <div class="news">
+          <h3>Anmeldung erfolgreich!</h3>
+          <?php echo $meldung; ?>
+        </div>
+<?php
+}
+?>
+      </div>
+<?php include_once("../includes/footer.html"); ?>
+    </div>
+  </body>
+</html>
---
+++ company/121128_sign-in.php
@@ -0,0 +1,193 @@
+<?php
+$array = explode ('/', $_SERVER['PHP_SELF']);
+$pfad = $array[1];
+$datei = $array[2];
+$array = explode('.', $datei);
+$datei = $array[0];
+require_once("../includes/funktionen.inc.php");
+
+// Variablen initialisieren
+$empfaenger = 'events@greenliff.com';
+//$empfaenger = 'uburri@netweaver.ch';
+$success = false;
+$fehler = false;
+$readonly = '';
+$fullname = '';
+$phone = '';
+$e_mail = '';
+$comment = '';
+
+
+if(isset($_REQUEST))
+{
+  $missbrauch = formularmissbrauch($_REQUEST);
+}
+// Courses
+if(isset($_POST['send']))
+{
+  // Formularmissbraeuche abfangen
+  $_POST = array_map('_filterData',$_POST);
+
+  if(isset($_POST['title']))
+  {
+    $title = $_POST['title'];
+  }
+  $fullname = $_POST['fullname'];
+  $phone = $_POST['phone'];
+  $e_mail = $_POST['e_mail'];
+  $comment = $_POST['comment'];
+
+  // fehlende Daten
+  if($fullname == '' || $phone == '' || $e_mail == '')
+  {
+    $meldung = '<p class="marker">Please fill in all required fields.</p>';
+    $fehler = true;
+  }
+  // E-Mail ist formal nicht korrekt
+  else if(!teste_email($e_mail))
+  {
+    $meldung = '<p class="marker">This E-mail seems to be invalid.</p>';
+    $fehler = true;
+  }
+  else
+  {
+    $extra = "MIME-Version: 1.0\nContent-Type: text/plain; charset=iso-8859-1\nContent-Transfer-Encoding: 8-bit\n";
+    $betreff = "Registration 28. November 2012: Crowd & Cloud based Testing";
+    $text = "\n\n";
+    $text .= 'Registration for the event "Crowd & Cloud based Testing" (28. November 2012)' . ":\n\n";
+    $text .= "Date.........: " . date("d.m.Y") . "\n";
+    $text .= "Full name....: " . $fullname . "\n";
+    $text .= "E-mail.......: " . $e_mail . "\n";
+    $text .= "Phone........: " . $phone;
+    if($comment != "")
+    {
+      $text .= "\n\nComment: \n" . $comment;
+    }
+    // print_r($text);
+  	// E-Mail-Versand
+    if(!$missbrauch)
+    {
+      /* Baut Header der Mail zusammen */
+      $headers = 'From:' . $empfaenger . "\n";
+      $headers .= 'X-Mailer: PHP/' . phpversion() . "\n";
+      $headers .= 'X-Sender-IP: ' . $REMOTE_ADDR . "\n";
+      $headers .= "Content-type: text/plain; charset=iso-8859-1\n";
+      $headers .= "Content-Transfer-Encoding: 8-bit\n";
+      $headers .= 'Cc: ' . $empfaenger . "\n";
+//echo nl2br($text);
+    	if(@mail($e_mail,$betreff,$text,$headers))
+      {
+        $meldung = '<p class="marker">Thank you for your registration.<br />You will receive a copy of the registration by e-mail.</p>';
+        $success = true;
+        $readonly = ' readonly="readonly"';
+      }
+      else {
+        $meldung = 'Ihre Anmeldung konnte nicht abgeschickt werden. Bitte <a href="mailto:info@greenliff.com">informieren Sie uns</a> über das Problem, so dass wir es beheben und Sie für den Anlass registrieren können.';
+        $fehler = true;
+      }
+    }
+
+  }
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <title>Greenliff: &laquo;Crowd &amp; Cloud based Testing&raquo; - PASS & Greenliff Afternoon Talks - Registration</title>
+		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
+		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+    <meta name="language" content="en" />
+		<meta name="robots" content="index,follow" />
+    <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
+    <!--[if IE 5]>
+    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <!--[if IE 6]>
+    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <!--[if IE 7]>
+    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
+    <![endif]-->
+    <link rel="shortcut icon" href="/favicon.ico" />
+  </head>
+
+  <body>
+    <div id="abstand"></div>
+    <div id="container">
+<?php include_once("../includes/printhead.php"); ?>
+      <div id="left">
+      <?php include_once("../includes/navigation.php"); ?>
+      </div>
+      <div id="middle">
+      </div>
+      <div id="scrollcontent" class="events">
+        <div class="startspalte">
+          <h1>Registration to the event</h1>
+          <h2>&laquo;Crowd &amp; Cloud based Testing&raquo; - PASS & Greenliff Afternoon Talks</h2>
+          
+          <p>Crowdsourcing and Cloud Computing have opened new dimensions in Software Testing. New possibilities like crowd testing, web based community platforms, cloud based virtual test systems and cloud based testing tools help us to further improve software quality assurance processes and methods and to specifically increase testing efficiency and effectiveness. Join us at &laquo;Crowd &amp; Cloud based On-Demand Testing&raquo; to get inspired, learn more about using cloud platforms and tools to manage crowdsourced testing teams. Understand the basic crowd testing principles and processes, receive first-hand information on concrete cases, and meet with our speakers and other industry professionals. This event has been organized by PASS and Greenliff, renown specialists in crowd testing and cloud based testing. Our guest speakers listed in the agenda of this afternoon session are recognized leaders in their specific field. Don't miss this unique opportunity, register today to reserve your seat in the &laquo;Clouds&raquo; in Zurich Prime Tower! (Due to international speakers this event will be held in english language.)</p>
+          <h3>Topics</h3>
+          <ul>
+            <li>The Crowd in the Cloud (Carl Esposti, Founder and CEO <a href="http://www.crowdsourcing.org" target="_blank">crowdsourcing.org</a>)</li>    
+            <li>eBay's Crowd Testing Strategy (Michael Palotas, Head Quality Engineering Europe, eBay International)</li>     
+            <li>How Crowd Testing works (Dieter Speidel, CEO PASS Group)</li>     
+            <li>Cloud Scale Testing (Joachim Büchse, Managing Director Greenliff)</li>           
+          </ul>
+
+          <a name="Registration"></a>
+          <form method="post" action="<?php echo $datei; ?>.php#Registration" style="margin-top: 30px;">
+          <h3>Bookings</h3>
+<?php
+          if(isset($meldung) && $meldung != '')
+{
+?>
+          <?php echo $meldung; ?>
+<?php
+}
+?>
+            <div class="div1">
+              <span class="formlabel">Price</span>CHF 190</span>
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $fullname == "") echo " marker"; ?>">Full name</span><input type="text" name="fullname"<?php echo $readonly; ?> class="formfeld" value="<?php echo $fullname; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $phone == "") echo " marker"; ?>">Phone</span><input type="text" name="phone"<?php echo $readonly; ?> class="formfeld" value="<?php echo $phone; ?>" />
+            </div>
+            <div class="div1">
+              <span class="formlabel required<?php if($fehler && $e_mail == "") echo " marker"; ?>">E-mail</span><input type="text" name="e_mail"<?php echo $readonly; ?> class="formfeld" value="<?php echo $e_mail; ?>" />
+            </div>
+            <div class="div1" style="margin-top: 30px;">
+              <span class="formlabel">Comment</span><textarea name="comment"<?php echo $readonly; ?> class="textarea" style="height:60px;"><?php echo $comment; ?></textarea>
+            </div>
+<?php
+if(!$success)
+{
+?>
+            <div class="div2">
+            <input type="submit" name="send" value="Send your booking" class="submit" />
+            </div>
+           </form>
+<?php
+}
+?>
+        </div>
+        <div class="news">
+          <h3>28. November 2012</h3>
+          <p><a href="http://primetower.ch/en/eat-meet/konferenzen" target="_blank">Clouds</a> Conference Center, Prime Tower Zürich</p>
+          <p>14:00 - 17:00h Apéro &amp; Networking &ndash; &laquo;Meet the Experts&raquo;</p>
+          <p>Cost: CHF 190.-</p>
+          <p>Audience: CIOs, R&D Managers, SW-Development Managers, Product Owners, Project Managers, Test Managers, Software Quality Managers</p>
+          <p><a href="../docs/Event_Agenda_Cloud_Crowd_based_On-Demand_Testing.pdf" target="_blank">more Information</a> (pdf)</p>
+          <p><a href="#Registration">Registration form</a></p>
+
+        </div>
+      </div>
+<?php include_once("../includes/footer.html"); ?>
+    </div>
+  </body>
+</html>
