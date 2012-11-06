09a833869074ecaa064e1cbcbf5762475e315733
Juraj Skripsky <js@hotfeet.ch>
2012-11-06 15:54:18 +0100

    Fix encoding, use minimum links to Greenliff.

--- company/news.php
+++ company/news.php
@@ -10,7 +10,7 @@ require_once("../includes/funktionen.inc.php");
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
     <title>Software Testing, Test Consultancy, Automation, Performance Testing, Zurich</title>
     <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
     <meta name="description" content="We are a fast growing company that manages challenging software testing projects. We are constantly looking for highly qualified individuals." />
@@ -35,7 +35,7 @@ require_once("../includes/funktionen.inc.php");
 			<h1>Greenliff and PASS Join Forces</h1>
 			05.11.2012<br /><br />
 			<p> 
-				<a href="http://www.pass.ch/" target="_blank">PASS Technologies</a> and <a href="/index.php">Greenliff</a> have stepped into a strategic partnership to extend their portfolio of QA and testing services.
+				<a href="http://www.pass.ch/" target="_blank">PASS Technologies</a> and <a href="/">Greenliff</a> have stepped into a strategic partnership to extend their portfolio of QA and testing services.
 				Since many years, both companies have been specialising in software quality assurance and application testing.
 				The partnership enables both companies to focus their expertise from different domains and disciplines and thereby to 
 				broaden their customer base. While Greenliff benefits by having access to PASS' crowdtesting platform
@@ -44,12 +44,12 @@ require_once("../includes/funktionen.inc.php");
 			<p>
 			<p> 
 				<a href="../docs/partnerschaft-pass-greenliff.pdf">Medienmittelung (PDF)</a><br />
-				<a href="../partners/index.php">Partner</a><br />
+				<a href="../partners/index.php">Partners</a><br />
 			</p> 
        </div>
          <div class="spalte2">
 			<p> 
-				<a href="http://www.pass.ch/" target="_blank">PASS Technologies</a> und <a href="/index.php">Greenliff</a> sind eine strategische Partnerschaft eingegangen, um bestehenden und neuen Kunden eine erweiterte 
+				<a href="http://www.pass.ch/" target="_blank">PASS Technologies</a> und <a href="/">Greenliff</a> sind eine strategische Partnerschaft eingegangen, um bestehenden und neuen Kunden eine erweiterte 
 				Palette von Testdienstleistungen anbieten zu können. Beide Firmen sind seit vielen Jahren Spezialisten rund um die Sicherstellung 
 				von Software-Qualität und das Testen von Software-Lösungen. Ziel der Partnerschaft ist es, die unterschiedliche Expertise hinsichtlich 
 				Branchen-Fokus und Dienstleistungsportfolio zu bündeln und einem breiteren Kundenkreis zugänglich zu machen. Während Greenliff durch 
