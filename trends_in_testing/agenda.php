e4ab655892f24e697fc56d98321e76c41262b9e2
Juraj Skripsky <js@hotfeet.ch>
2013-01-11 15:23:10 +0100

    Prepare pages for 'trends-in-testing'.

--- trends_in_testing/agenda.php
+++ trends_in_testing/agenda.php
@@ -5,29 +5,15 @@ $datei = $array[2];
 $array = explode('.', $datei);
 $datei = $array[0];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>Greenliff: Trends in Testing 2012 - Referenten</title>
-		<meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Fast, Zurich, Zuerich, Zürich" />
-		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
-    <meta name="language" content="en" />
-		<meta name="robots" content="index,follow" />
+	<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/trendsintesting.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <!--[if IE 5]>
-    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 6]>
-    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_once("../includes/google-analytics.html"); ?>
   </head>
@@ -45,8 +31,8 @@ $datei = $array[0];
         <h1>Agenda und Vorträge</h1>
 
         <div style="margin-top:10px;">
-          <h2>Datum: 03. April 2012</h2>
-          <p><strong>Ort:</strong> World Trade Center Zürich, Leutschenbachstr. 95, 8050 Zürich</p>
+          <h2>Datum: 19. März 2013 </h2>
+          <p><strong>Ort:</strong> Holiday Inn Oerlikon, Zürich Oerlikon</p>
           <p>Die Veranstaltung ist für Sie kostenlos. Allen Teilnehmern stehen die Vortragsfolien nach der Veranstaltung im Downloadbereich zur Verfügung.</p>
           <table>
             <tr>
@@ -54,68 +40,100 @@ $datei = $array[0];
               <td>Registrierung</td>
             </tr>
             <tr>
-              <td>09:30 - 09:45</td>
+              <td>09:30 - 09:40</td>
               <td>Begrüssung</td>
             </tr>
             <tr>
-              <td class="tdSpalte tablehead" style="width:100px;">09:45 - 10:30</td>
-              <td class="tdSpalte tablehead">Warum ist &laquo;agil&raquo; so sexy? </td>
+              <td class="tdSpalte tablehead" style="width:100px;">09:40 - 10:00</td>
+              <td class="tdSpalte tablehead">Warum können Normen helfen?</td>
             </tr>
+
             <tr>
               <td></td>
               <td>
-                <p>Agile Softwareentwicklung adressiert neue Werte. Dokumente und Prozesse treten zugunsten enger Zusammenarbeit im Team mit kurzen Feedbackschleifen in den Hintergrund. </p>
-                <p>Der Vortrag motiviert die Diskussion über die richtige Balance zwischen den attraktiven neuen Werten und bewährter Methodik und eröffnet den Trends in Testing-Vortragsbogen aus Erfahrungsbericht, Testkompetenz und richtigem Tooleinsatz.</p>
+<p>Der am häufigsten genannte Grund, Normen zu nutzen, ist die Erfüllung gesetzlicher Vorgaben, z.B. Zertifizierungsvorschriften.</p>
+<p>Damit wird das Thema schnell unliebsam und der Nutzen angesichts des Zwangs oft nicht entsprechend gewertet. Dieser Eröffnungsbeitrag motiviert zu erkennen, dass die Nutzung vorhandener Expertise immer ein Gewinn ist; auch für diejenigen, die nicht gesetzlich dazu "verdonnert" sind. Gerade das Qualitätsmanagement kann als Unterstützungsprozess ideal vom Wissen vorheriger Spezialisten-Generationen partizipieren und damit dem Unternehmen Freiräume schaffen, sich auf die Kernkompetenzen in ihrer Domäne zu konzentrieren.</p>
+<p>Normen sind ein exzellentes Vehikel, mit  dem sich dieses Spezialistenwissen in die eigene Firma transportieren lässt.</p>
+<p>Das Rad neu zu erfinden, lohnt sich eben nur für neue Räder!</p> 
               </td>
             </tr>
             <tr>
-              <td>10:30 - 11:00</td>
+              <td>10:00 - 10:30</td>
               <td>Pause &ndash; "Meet the Experts"</td>
             </tr>
+
             <tr>
-              <td class="tdSpalte tablehead" style="width:100px;">11:00 - 12:00</td>
-              <td class="tdSpalte tablehead">Agil Testen: Von &laquo;Test-First&raquo; bis &laquo;Test nonstop&raquo;</td>
+              <td class="tdSpalte tablehead" style="width:100px;">10:30 - 11:30</td>
+              <td class="tdSpalte tablehead">Der Weg zur Norm</td>
             </tr>
             <tr>
               <td></td>
               <td>
-                <p>Testen in agilen Projekten ist anspruchsvoll &ndash; mit Testkompetenz und Methodenwissen findet man den richtigen Ansatz. Die wesentlichen Bausteine dieses Ansatzes sind u. a. das &laquo;Test First&laquo;-Konzept aus dem &laquo;eXtreme-Programming&raquo; und das Prinzip des &laquo;Test nonstop&raquo; für den kontinuierlichen Systemtest.</p>
-                <p>Der Vortrag öffnet diesen &laquo;Bausteinkatalog&laquo;, vermittelt Verständnis und erforderliche Theorie für das Setup des Tests in konkreten, agilen Projekten.</p>
-              </td>
+<p>Es ist offensichtlich - die Normungsgremien dieser Welt sind höchst produktiv. Eine kaum überschaubare Anzahl von Standards wird jährlich verabschiedet, überarbeitet und veröffentlicht.</p>
+<p>Viele davon betreffen die Arbeit in der Sofwarebranche, einige spezifisch den Softwaretest.</p>
+<p>Wie kommt es zu einem Standard, und wer ist am Entwicklungsprozess beteiligt?</p>
+<p>In diesem Vortrag geht es um den Weg von der Motivation zur Erstellung von Standards um Ideen, den Entscheidungsweg bis hin zum Lebenszyklus des Standards, vor dem Hintergrund nationaler und internationaler Gremien und deren Zusammenarbeit.</p>
+				</td>
+            </tr>
+
+            <tr>
+              <td class="tdSpalte tablehead" style="width:100px;">11:30 - 12:30</td>
+              <td class="tdSpalte tablehead">ISO/IEC 29119 - Juwelen des Softwaretest</td>
             </tr>
             <tr>
-              <td>12:00 - 13:30</td>
+              <td></td>
+              <td>
+<p>Als Füllhorn, voll von Anregungen für Prozesse und Dokumente, bereichert sie jeden Software Entwicklungsprozess.</p>
+<p>Ihre Ideen und Konzepte bringen Impulse für Arbeit und Erfolg der Projekte. Aber woraus besteht der Schatz ISO/IEC 29119?</p>
+<p>Wie kann man ihn finden? Dieser Vortrag führt zur Schatztruhe. Lassen Sie sich die Kostbarkeiten erklären und nutzen Sie die ISO/IEC 29119 als Leitfaden zur Erreichung wirtschaftlicher und qualitativ hochwertiger Projektergebnisse.</p>
+              </td>
+            </tr>
+
+
+            <tr>
+              <td>12:30 - 14:00</td>
               <td>Mittagspause &ndash; "Meet the Experts"</td>
             </tr>
+
             <tr>
-              <td class="tdSpalte tablehead" style="width:100px;">13:30 - 14:30</td>
-              <td class="tdSpalte tablehead">Erfahrungen eines &laquo;Sprinters&raquo;</td>
+              <td class="tdSpalte tablehead" style="width:100px;">14:00 - 15:00</td>
+              <td class="tdSpalte tablehead">DIN ISO IEC 29119: Nitro oder Bremse?</td>
             </tr>
             <tr>
               <td></td>
               <td>
-                <p>Wie führt man erfolgreich agile Methoden im Projekt ein? Worauf kommt es an? Welche Fallen gibt es? Was muss man bei der Zusammenstellung der agilen Teams berücksichtigen?</p>
-                <p>imbus hat mit seinem Entwicklungsteam für das Testmanagement-Werkzeug TestBench diesen Weg beschritten. Diskutieren Sie mit, wenn im Rahmen dieses Erfahrungsberichts aufgezeigt wird, welche Entscheidungen zu treffen waren und wie der Prozess gestaltet wurde.</p>
+<p>Nach vielen Jahren Arbeit im ISO/IEC Normenausschuss JTC1 SC7 ist es nun soweit:</p>
+<p>Der neue, vierteilige Standard ISO/IEC 29119 steht kurz vor der Fertigstellung, und wird, wie IEEE 829 und BS 7925 vor ihm, zum langjährigen Wegbegleiter der Softwaretest-Experten werden.</p>
+<p>Was bringt der neue Standard mit sich?</p>
+<p>Dieser Vortrag beantwortet die praktischen Fragen der neuen Norm, mit dem Schwerpunkt Softwaretest-Dokumentation:<br />
+	Welche Dokumente machen beim Testen Sinn? Wie hängen diese Dokumente zusammen?
+</p>
+<p>Was sind die Inhalte des Standards, und wie lässt sich dieser  in einem klassischen oder beispielsweise in einem agilen Prozess anwenden?</p>
               </td>
             </tr>
+
             <tr>
-              <td>14:30 - 15:00</td>
+              <td>15:00 - 15:30</td>
               <td>Pause &ndash; "Meet the Experts"</td>
             </tr>
+
             <tr>
-              <td class="tdSpalte tablehead" style="width:100px;">15:00 - 16:30</td>
-              <td class="tdSpalte tablehead">Test-nonstop &ndash; Live Demo</td>
+              <td class="tdSpalte tablehead" style="width:100px;">15:30 - 16:45</td>
+              <td class="tdSpalte tablehead">Gibt es das? Normenkonform arbeiten und berichten ohne Mühe?</td>
             </tr>
             <tr>
               <td></td>
               <td>
-                <p>In der Live-Demo sehen Sie, wie man Systemtest nonstop mit TestBench realisiert. Parallel zur Entwicklung &ndash; im selben Sprint &ndash; werden die Systemtests spezifiziert, automatisiert und laufen täglich vollautomatisiert als Teil der Continuous Integration oder im Nightly-Build. Dabei können beliebige Testautomaten angesteuert werden.</p>
-                <p>Durch die zentrale Sammlung aller Test-Informationen im TestBench Repository entsteht dabei &laquo;en passant&raquo; eine vollständige Dokumentation der Qualitätssicherung. Auch anspruchsvolle Forderungen nach regulatorischer Konformität werden so sicher erfüllt.</p>
+<p>Landläufig wird meist festgestellt, dass normenkonformes Arbeiten einher geht mit vielen extra Aufwänden zum Nachweis der Konformität, die man ohne die Normen nicht hätte.</p>
+<p>Hierbei gilt als einer der Aufwandstreiber das Berichtswesen bzw. das Bereitstellen korrekter und ausreichender Daten für diese Berichte, die die notwendigen Nachweise liefern sollen. So mancher Testmanager kennt die Situation, dass er erst lange Informationen erheben und konsolidieren muss, bis er einen aussagekräftigen Bericht erzeugen kann.</p>
+<p>Wenn die Prüfung der Daten auf Korrektheit und ausreichend für die Berichte erst kurz vor der Erstellung der Berichte oder gar erst auf Basis der erstellten Berichte stattfindet, ist das sicher immer wieder der Fall.</p>
+<p>Im Blick auf ein schnelles, schlankes und hinsichtlich der Normenkonformität korrektes Berichtswesen lohnt sich der Einsatz von Werkzeugen und ein frühzeitiges Prüfen der zugrunde liegenden Daten.</p>
+<p>Wie so oft ist der richtige Werkzeugeinsatz an der richtigen Stelle entscheidend, der fast nebenbei ein schnelles, vollständiges und laufend wiederholbares Berichtswesen ermöglicht.</p> 
               </td>
             </tr>
             <tr>
-              <td>ab 16:30 Uhr</td>
-              <td>Apéro</td>
+              <td>ab 16:45 Uhr</td>
+              <td>Closing mit anschließendem Get Together</td>
             </tr>
           </table>
 
--- trends_in_testing/index.php
+++ trends_in_testing/index.php
@@ -5,29 +5,17 @@ $datei = $array[2];
 $array = explode('.', $datei);
 $datei = $array[0];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <title>Greenliff: Agile Testing, Test nonstop, Softwaretest</title>
-    <meta name="keywords" content="Greenliff, Software Testing, Test Consultancy, Test Automation, Performance Testing, Test Case, Test Tool Development, Financial Application Testing, Mobile Application Testing, Test Oursourcing, Agile Testing, Test nonstop, Softwaretest, Fast, Zurich, Zuerich, Zürich, Agile Testing, Test nonstop, Softwaretest" />
-		<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
+	<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
     <meta name="language" content="en" />
-		<meta name="robots" content="index,follow" />
+	<meta name="robots" content="index,follow" />
     <link rel="stylesheet" type="text/css" href="/styles/screen.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/trendsintesting.css" media="screen" />
     <link rel="stylesheet" type="text/css" href="/styles/print.css" media="print" />
-    <!--[if IE 5]>
-    <link href="/styles/ie5.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 6]>
-    <link href="/styles/ie6.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
-    <!--[if IE 7]>
-    <link href="/styles/ie7.css" rel="stylesheet" type="text/css" media="screen" />
-    <![endif]-->
     <link rel="shortcut icon" href="/favicon.ico" />
      <?php include_once("../includes/google-analytics.html"); ?>
  </head>
@@ -43,16 +31,30 @@ $datei = $array[0];
       </div>
       <div id="content">
         <div class="spalte1">
-          <h1>Trends in Testing 2012</h1>
-          <p>Time-to-Market ist dominierender Wettbewerbsfaktor &ndash; auch und besonders bei Softwareprodukten. Durch Einsatz agiler Methoden kann die Time-to-Market erheblich verkürzt werden. Der Entwicklungszyklus komprimiert sich bis auf ein- bis drei-wöchige Sprints.</p>
-          <a href="/images/trends_in_testing/agile_testing.jpg"><img src="/images/trends_in_testing/agile_testing_small.jpg" alt="Poster Agile Testing" width="173" height="121" /></a>
+          <h1>Trends in Testing 2013</h1>
+          <p>
+				Qualität ist ein maßgeblicher Wettbewerbsfaktor für Softwareprodukte. Die steigenden Anforderungen an den IT-Markt sind untrennbar mit Qualitätssicherung und Test verbunden.
+          </p>
 
+          <p>
+				Immer komplexer werdende Entwicklungsprozesse müssen in kurzen Zeitabständen bewältigt werden, was auch die Ansprüche an die Testbranche kontinuierlich wachsen lässt.
+				Tests müssen vergleichbar, messbar und auch für Fachbereichsmitarbeiter verständlich sein, häufig auch, um gesetzliche Vorgaben erfüllen zu können.
+          </p>
+
+          <p>
+				Lassen Sie sich im Rahmen der <strong>Trends in Testing 2013</strong> erklären, wie Sie die in Standards verfügbare jahrzehntelange Testerfahrung vieler Testspezialisten sofort für sich und Ihr Unternehmen nutzen können.
+          </p>
         </div>
 
         <div class="spalte2">
+          <p class="abstandOben">
+				Der Standard arbeitet für Sie! Holen Sie sich Rückendeckung für eine klare Struktur in Ihrem Testprozess.
+          </p>
 
-          <p class="abstandOben">QS und Test müssen mit dieser höheren &laquo;Taktfrequenz&raquo; Schritt halten. Die Tester müssen Features in kurzen Intervallen prüfen und ihr Feedback unmittelbar geben können. Testen findet nonstop und automatisiert statt. Trends in Testing 2012 zeigt Ihnen, wie Sie Test nonstop in Ihrem Projekt implementieren, auch wenn Sie nicht agil entwickeln, sondern z.B. nach V-Modell, kann Test nonstop Integrations- und Systemtest deutlich beschleunigen.</p>
-        </div>
+          <p>
+				Unsere Experten nehmen den Normen ihren vorurteilsbehafteten Ruf von "trockener Materie" und zeigen, dass jedes Projekt, ob klassisch angelegt oder agil durchgeführt, von Standards, wie etwa <strong>ISO/IEC 29119</strong>, partizipieren kann.
+			</p>
+		</div>
 
         <div class="news">
           <h3>Über Trends in Testing</h3>
