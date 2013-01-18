ea3a5a090a41ea1e20cd3b7daf2a135ecd211115
Juraj Skripsky <js@hotfeet.ch>
2013-01-18 11:38:33 +0100

    Fix encoding.

--- trends_in_testing/index.php
+++ trends_in_testing/index.php
@@ -8,7 +8,7 @@ $datei = $array[0];
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
     <title>Greenliff: Agile Testing, Test nonstop, Softwaretest</title>
 	<meta name="description" content="Greenliff is an independent Engineering and Service company focused on software testing and quality control. Serving customers in the telecom, the industrial, the medical and the finance sectors, we specialize in test consultancy, test automation and the development of test solutions covering the entire software life cycle from specification to maintenance." />
     <meta name="language" content="en" />
